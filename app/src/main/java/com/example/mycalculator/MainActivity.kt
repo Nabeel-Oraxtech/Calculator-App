9e376bb753da677a7f4698cc374057c84dddf76c
Nabeel Ahmad <nabeel.oraxtech@gmail.com>
2023-12-08 12:32:59 +0500

    Finalized the Calculator App

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var tv:TextView?=null
@@ -31,6 +30,71 @@ class MainActivity : AppCompatActivity() {
          Lastnumber=false
          Lastdot=true
       }
+    }
+    fun equal(view: View){
+        if(Lastnumber){
+             var tvValue=tv?.text.toString()
+            var prefix=""
+            try {
+                if(tvValue.startsWith("-")){
+                     prefix = "-"
+                    tvValue=tvValue.substring(1)
+                }
+                if (tvValue.contains("-")){
+                    var splitValue=tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one= prefix + one
+                    }
+                    tv?.text= removezero((one.toDouble()-two.toDouble()).toString())
+                }
+                else  if (tvValue.contains("+")){
+                    var splitValue=tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one= prefix + one
+                    }
+                    tv?.text= removezero((one.toDouble()+two.toDouble()).toString())
+                }
+                else  if (tvValue.contains("*")){
+                    var splitValue=tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one= prefix + one
+                    }
+                    tv?.text= removezero((one.toDouble()*two.toDouble()).toString())
+                }
+                else  if (tvValue.contains("/")){
+                    var splitValue=tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one= prefix + one
+                    }
+                    tv?.text= removezero((one.toDouble()/two.toDouble()).toString())
+                }
+
+
+            }
+            catch (e:ArithmeticException){
+                e.printStackTrace()
+
+            }
+        }
+    }
+    fun removezero(result: String):String{
+        var value=result
+        if(result.contains(".0")){
+            value=result.substring(0,result.length-2)
+        }
+        return value
     }
     fun operator(view: View){
         if (Lastnumber && !operateadded(tv?.text.toString())){
